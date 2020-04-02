678749789211915af1af9b51f8ebaccb50a2dc88
LobodaAndrey <lobodaandrey92@gmail.com>
2020-04-02 14:09:09 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="post" v-for="post in posts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3')
+    const posts = await res.json();
+    this.posts = posts;
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  width: 400px;
+}
+.post {
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+
+</style>
